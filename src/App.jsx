31386b3ac18e3342f5cd169028626ee26b1d25d3
falahal24.si@mahasiswa.pcr.ac.id <falahal24si@mahasiswa.pcr.ac.id>
2026-04-28 08:03:46 +0700

    pertemuan 7 membuatAuthLayout

--- src/App.jsx
+++ src/App.jsx
@@ -1,80 +1,35 @@
 import "./assets/tailwind.css";
-import Sidebar from "./layouts/Sidebar";
-import Header from "./layouts/Header";
-import Dashboard from "./pages/Dashboard";
+// import Dashboard from "./pages/Dashboard";
 import NotFound from "./pages/NotFound";
 import Orders from "./pages/Orders";
 import Customers from './pages/Customers';
 import { Route, Routes } from "react-router-dom";
 import ErrorPage from "./components/ErrorPage";
+import MainLayout from "./layouts/MainLayout";
+import AuthLayout from "./layouts/AuthLayout";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import Forgot from "./pages/auth/Forgot";
 
 function App() {
-  // const [count, setCount] = useState(0)
 
   return (
-    <div className='flex min-h-screen w-full bg-#f3f4f6'>
-        <Sidebar/>
-  
-  <div className="flex-1 p-4">
-    <Header />
-    
     <Routes>
-        <Route path="*"element={<NotFound/>} />
+      <Route element={<MainLayout/>}>
+        <Route path="*" element={<NotFound/>} />
         <Route path="/" element={<Dashboard />} />
         <Route path="/orders" element={<Orders />} />
         <Route path="/customers" element={<Customers />} />
-
         <Route path="/" element={<Dashboard />} />
+      </Route>
 
-  <Route 
-    path="/error-400" 
-    element={
-      <ErrorPage
-        code="400"
-        message="Bad Request"
-        image="/images/400.png"
-      />
-    }
-  />
-
-  <Route 
-    path="/error-401" 
-    element={
-      <ErrorPage
-        code="401"
-        message="Unauthorized Access"
-        image="/images/401.png"
-      />
-    }
-  />
-
-  <Route 
-    path="/error-403" 
-    element={
-      <ErrorPage
-        code="403"
-        message="Forbidden Access"
-        image="/images/403.png"
-      />
-    }
-  />
-
-  {/* 404 */}
-  <Route 
-    path="*" 
-    element={
-      <ErrorPage
-        code="404"
-        message="Page Not Found"
-        image="/images/404.png"
-      />
-    }
-  />
+      <Route element={<AuthLayout/>}>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register/>} />
+        <Route path="/forgot" element={<Forgot/>} />
+      </Route>
     </Routes>
-</div>
-</div>
   )
-       
 }
 
-export default App
+export default App;
